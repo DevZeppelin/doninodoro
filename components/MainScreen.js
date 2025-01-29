bd5cbe67938819bde52371a958cd43d1a31c243b
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2025-01-29 09:57:34 -0300

    try resolve safari problem with useState in MainScreen

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
+
 const MainScreen = () => {
+
+  const [bgClass, setBgClass] = useState("bg-fixed");
+
+  useEffect(() => {
+    if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+      setBgClass("bg-local");
+    }
+  }, []);
+
   return (
-    <div className="h-[90vh] md:h-[60vh] bg-hero-mobile2 md:bg-hero-pattern bg-fixed bg-cover flex items-center md:justify-end">
+    // <div className="h-[90vh] md:h-[60vh] bg-hero-mobile2 md:bg-hero-pattern bg-fixed bg-cover flex items-center md:justify-end">
+    <div className={`h-[90vh] md:h-[60vh] bg-hero-mobile2 md:bg-hero-pattern ${bgClass} bg-cover flex items-center md:justify-end`}>
+    
       <span
         className="text-secundary text-5xl md:text-6xl p-8 md:p-16 text-right"
         style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.8)" }}
